7701b2d584aa42d8d32b7ac8a0426999cd3dc9c1
Ryan Martins <ryanbarroso2017@gmail.com>
2026-07-29 13:46:07 -0300

    refactor(meeting): separating EmptyMeetings component from Meeting page

---
+++ src/components/empty-meetings.tsx
@@ -0,0 +1,37 @@
+import { CircleQuestionMark } from "lucide-react";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "./ui/empty";
+import { DialogMeeting } from "./dialog-meeting";
+import { Button } from "./ui/button";
+
+export function EmptyMeetings() {
+  return (
+    <Empty className="md:max-w-md lg:max-w-lg justify-self-center border border-dashed border-gray-300">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <CircleQuestionMark></CircleQuestionMark>
+        </EmptyMedia>
+      <EmptyTitle>Nenhuma monitoria encontrada!</EmptyTitle>
+      <EmptyDescription>
+        Você não tem nenhuma Monitoria marcada. Agende uma monitoria e ela
+        aparecerá aqui.
+      </EmptyDescription>
+    </EmptyHeader>
+    <EmptyContent>
+      <DialogMeeting>
+        <Button variant="outline" size="sm">
+          Agendar Monitoria
+        </Button>
+      </DialogMeeting>
+    </EmptyContent>
+  </Empty>
+  )
+}
+
+export default EmptyMeetings;
--- src/pages/meetings/Metting.tsx
+++ src/pages/meetings/Metting.tsx
@@ -1,14 +1,7 @@
 import { Button } from "@/components/ui/button";
-import {
-  Empty,
-  EmptyContent,
-  EmptyDescription,
-  EmptyHeader,
-  EmptyMedia,
-  EmptyTitle,
-} from "@/components/ui/empty";
-import { CircleQuestionMark, Pencil } from "lucide-react";
+import { Pencil } from "lucide-react";
 import { DialogMeeting } from "../../components/dialog-meeting";
+import { EmptyMeetings } from "@/components/empty-meetings";
 
 function Meeting() {
   return (
@@ -23,26 +16,18 @@ function Meeting() {
           </Button>
         </DialogMeeting>
       </div>
+
       <div className="p-6 flex justify-center">
-        <Empty className="md:max-w-md lg:max-w-lg justify-self-center border border-dashed border-gray-300">
-          <EmptyHeader>
-            <EmptyMedia variant="icon">
-              <CircleQuestionMark></CircleQuestionMark>
-            </EmptyMedia>
-            <EmptyTitle>Nenhuma monitoria encontrada!</EmptyTitle>
-            <EmptyDescription>
-              Você não tem nenhuma Monitoria marcada. Agende uma monitoria e ela
-              aparecerá aqui.
-            </EmptyDescription>
-          </EmptyHeader>
-          <EmptyContent>
-            <DialogMeeting>
-              <Button variant="outline" size="sm">
-                Agendar Monitoria
-              </Button>
-            </DialogMeeting>
-          </EmptyContent>
-        </Empty>
+        <EmptyMeetings />
+      </div>
+
+      <div className="flex-1 p-6">
+        <h2 className="text-lg font-semibold">Monitorias Agendadas</h2>
+        <div className="mt-4">
+          <p className="text-sm text-gray-500">
+            Nenhuma monitoria agendada no momento.
+          </p>
+        </div>
       </div>
     </div>
   );
