9b4eb4732b07e214d792790a16f4d4ccf1c53e89
Ryan Martins <ryanbarroso2017@gmail.com>
2026-08-12 14:08:28 -0300

    feat(types): add UsuarioRequest and UsuarioResponse types for user management

---
+++ src/types/usuarios.ts
@@ -0,0 +1,12 @@
+import type { Materia } from "./materia";
+
+export type UsuarioRequest = {
+  nome: string;
+  materia: Materia;
+};
+
+export type UsuarioResponse = {
+  id: string;
+  nome: string;
+  materia: Materia;
+};
