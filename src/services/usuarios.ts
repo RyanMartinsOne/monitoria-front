2608323e6aed3adbcc7a442ea2a3e6d70beaee12
Ryan Martins <ryanbarroso2017@gmail.com>
2026-08-21 11:27:33 -0300

    refactor(services): remove createUsuario function from usuarios service

--- src/services/usuarios.ts
+++ src/services/usuarios.ts
@@ -1,10 +1,5 @@
 import { api } from "../lib/api";
-import type { UsuarioRequest, UsuarioResponse } from "../types/usuarios";
-
-export async function createUsuario(data: UsuarioRequest) {
-  const response = await api.post("/usuarios", data);
-  return response.data;
-}
+import type { UsuarioResponse } from "../types/usuarios";
 
 export async function getMe(): Promise<UsuarioResponse> {
   const response = await api.get("/usuario/eu");
