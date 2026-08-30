ca64e1eb76427016bbfd00117fd073a1e3424cd4
Ryan Martins <ryanbarroso2017@gmail.com>
2026-08-30 15:58:46 -0300

    feat(hooks): add new queries to useDashboard
    - Sorting useEncontroById data
    - Encontro search with filters query
    - UsuarioById query

--- src/hooks/useDashboard.ts
+++ src/hooks/useDashboard.ts
@@ -1,5 +1,11 @@
-import { getEncontroById, getUsuarios } from "@/services/dashboard";
-import type { EncontroResponse } from "@/types/encontro";
+import {
+  getEncontroById,
+  getEncontroWithFilters,
+  getUsuarioById,
+  getUsuariosByMateria,
+} from "@/services/dashboard";
+import type { EncontroFilter, EncontroResponse } from "@/types/encontro";
+import type { Materia } from "@/types/materia";
 import type { UsuarioResponse } from "@/types/usuarios";
 import { useQuery } from "@tanstack/react-query";
 
@@ -7,12 +13,32 @@ export function useEncontroById(id: string) {
   return useQuery<EncontroResponse[]>({
     queryKey: ["encontro", id],
     queryFn: () => getEncontroById(id),
+    select: (data) => {
+      // Ordena do mais recente ao mais antigo
+      return [...data].sort((a, b) => {
+        return new Date(b.dataHora).getTime() - new Date(a.dataHora).getTime();
+      });
+    },
   });
 }
 
-export function useUsuarios() {
+export function useEncontroWithFilters(filter: EncontroFilter) {
+  return useQuery<EncontroResponse[]>({
+    queryKey: ["encontros", filter],
+    queryFn: () => getEncontroWithFilters(filter),
+  });
+}
+
+export function useUsuarioById(id: string) {
+  return useQuery<UsuarioResponse>({
+    queryKey: ["usuarios", id],
+    queryFn: () => getUsuarioById(id),
+  });
+}
+
+export function useUsuarios(materia?: Materia) {
   return useQuery<UsuarioResponse[]>({
-    queryKey: ["usuarios"],
-    queryFn: () => getUsuarios(),
+    queryKey: ["usuarios", materia],
+    queryFn: () => getUsuariosByMateria(materia),
   });
 }
--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,5 +1,6 @@
 import { api } from "@/lib/api";
-import type { EncontroResponse } from "@/types/encontro";
+import type { EncontroFilter, EncontroResponse } from "@/types/encontro";
+import type { Materia } from "@/types/materia";
 import type { UsuarioResponse } from "@/types/usuarios";
 
 export async function getEncontroById(id: string): Promise<EncontroResponse[]> {
@@ -7,7 +8,48 @@ export async function getEncontroById(id: string): Promise<EncontroResponse[]> {
   return response.data;
 }
 
-export async function getUsuarios(): Promise<UsuarioResponse[]> {
-  const response = await api.get("/dashboard/usuarios");
+export async function getEncontroWithFilters(
+  filter: EncontroFilter,
+): Promise<EncontroResponse[]> {
+  const params = new URLSearchParams();
+
+  if (filter.status) {
+    params.set("status", filter.status);
+  }
+
+  if (filter.materia) {
+    params.set("materia", filter.materia);
+  }
+
+  const queryString = params.toString();
+
+  const response = await api.get(
+    queryString
+      ? `/dashboard/encontros?${queryString}`
+      : "/dashboard/encontros",
+  );
+
+  return response.data;
+}
+
+export async function getUsuarioById(id: string): Promise<UsuarioResponse> {
+  const response = await api.get(`/dashboard/usuarios/${id}`);
+  return response.data;
+}
+
+export async function getUsuariosByMateria(
+  materia?: Materia,
+): Promise<UsuarioResponse[]> {
+  const params = new URLSearchParams();
+  if (materia) {
+    params.set("materia", materia);
+  }
+
+  const queryString = params.toString();
+
+  const response = await api.get(
+    queryString ? `/dashboard/usuarios?${queryString}` : "/dashboard/usuarios",
+  );
+
   return response.data;
 }
