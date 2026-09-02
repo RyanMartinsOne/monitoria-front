7421716fcb23cd030642acc0ddf41b5198f6b6c8
Ryan Martins <ryanbarroso2017@gmail.com>
2026-09-02 08:38:14 -0300

    refactor(empty-meetings): export default and update import path for DialogMeeting

--- src/components/empty-meetings.tsx
+++ src/components/empty-meetings.tsx
@@ -7,31 +7,29 @@ import {
   EmptyMedia,
   EmptyTitle,
 } from "./ui/empty";
-import { DialogMeeting } from "./dialog-meeting";
+import { DialogMeeting } from "./meeting/encontro-form";
 import { Button } from "./ui/button";
 
-export function EmptyMeetings() {
+export default function EmptyMeetings() {
   return (
     <Empty className="md:max-w-md lg:max-w-lg justify-self-center border border-dashed border-gray-300">
       <EmptyHeader>
         <EmptyMedia variant="icon">
           <CircleQuestionMark></CircleQuestionMark>
         </EmptyMedia>
-      <EmptyTitle>Nenhuma monitoria encontrada!</EmptyTitle>
-      <EmptyDescription>
-        Você não tem nenhuma Monitoria marcada. Agende uma monitoria e ela
-        aparecerá aqui.
-      </EmptyDescription>
-    </EmptyHeader>
-    <EmptyContent>
-      <DialogMeeting>
-        <Button variant="outline" size="sm">
-          Agendar Monitoria
-        </Button>
-      </DialogMeeting>
-    </EmptyContent>
-  </Empty>
-  )
+        <EmptyTitle>Nenhuma monitoria encontrada!</EmptyTitle>
+        <EmptyDescription>
+          Você não tem nenhuma Monitoria marcada. Agende uma monitoria e ela
+          aparecerá aqui.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <DialogMeeting>
+          <Button variant="outline" size="sm">
+            Agendar Monitoria
+          </Button>
+        </DialogMeeting>
+      </EmptyContent>
+    </Empty>
+  );
 }
-
-export default EmptyMeetings;
