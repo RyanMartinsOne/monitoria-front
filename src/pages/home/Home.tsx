8118af445c12de201c211fb4ece37c04610e7264
Ryan Martins <ryanbarroso2017@gmail.com>
2026-09-02 08:45:41 -0300

    feat(home): replace table with EncontroCard and EncontroTable for displaying scheduled meetings

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,57 +1,96 @@
-import { Button } from "@/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { EyeIcon } from "lucide-react";
- 
+import EncontroCard from "@/components/meeting/encontro-card";
+import EncontroTable from "@/components/meeting/encontro-table";
+import { toast } from "@/components/ui/toast";
+import { useEncontros, useUpdateEncontroStatus } from "@/hooks/useEncontro";
+import { useCurrentUser } from "@/hooks/useUsuarios";
+import type { StatusEncontro } from "@/types/encontro";
+
 export default function Home() {
+  const { data: usuario } = useCurrentUser();
+  const { data: encontros = [], isLoading, isError } = useEncontros("AGENDADO");
+  const { mutate: updateStatus, isPending: isUpdatingStatus } =
+    useUpdateEncontroStatus();
+
+  function handleStatusChange(id: string, novoStatus: StatusEncontro) {
+    updateStatus(
+      {
+        encontroId: id,
+        status: novoStatus,
+      },
+      {
+        onSuccess: () => {
+          toast.add({
+            title: `Encontro ${novoStatus.toLocaleLowerCase()}`,
+            description: `O status do encontro foi marcado como ${novoStatus.toLocaleLowerCase()}.`,
+            type: "success",
+          });
+        },
+
+        onError: () => {
+          toast.add({
+            title: "Erro ao atualizar status",
+            description: "Não foi possível atualizar o status do encontro.",
+            type: "error",
+          });
+        },
+      },
+    );
+  }
+
   return (
     <div className="w-full p-6 flex flex-col items-center space-y-7">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight">Página Inicial</h1>
-        <p className="mt-2 text-base">Bem-vindo à plataforma de monitoria!</p>
+        <p className="mt-2 text-base">Bem-vindo, {usuario?.nome}</p>
       </div>
 
       <div className="w-full space-y-4">
         <h2 className="text-xl font-semibold">Próximos Encontros</h2>
 
-        <div className="w-full overflow-x-auto rounded-xl border">
-          <Table>
-            <TableCaption className="text-mauve-700">
-              Histórico de monitorias agendadas.
-            </TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-45">Aluno</TableHead>
-                <TableHead>Matéria</TableHead>
-                <TableHead>Assunto</TableHead>
-                <TableHead>Horário</TableHead>
-                <TableHead>Data</TableHead>
-                <TableHead className="text-right">Detalhes</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">Brian O' Conner</TableCell>
-                <TableCell>Matemática</TableCell>
-                <TableCell>Progressão Geométrica</TableCell>
-                <TableCell>10:00 - 12:00</TableCell>
-                <TableCell>2023-10-15</TableCell>
-                <TableCell className="text-right">
-                  <Button variant="outline" size="sm">
-                    <EyeIcon />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </div>
+        {isLoading && (
+          <div className="pt-2 text-sm text-muted-foreground">
+            Carregando encontros...
+          </div>
+        )}
+
+        {isError && (
+          <div className="pt-2 text-sm text-destructive">
+            Ocorreu um erro ao carregar as monitorias. Por favor, tente
+            novamente mais tarde.
+          </div>
+        )}
+
+        {!isLoading && !isError && encontros && encontros.length === 0 && (
+          <div className="pt-2 text-sm text-muted-foreground">
+            Nenhuma monitoria agendada para os próximos dias.
+          </div>
+        )}
+
+        {!isLoading && !isError && encontros && encontros.length > 0 && (
+          <div className="w-full overflow-x-auto rounded-xl border">
+            {/* Visualização mobile */}
+            <div className="space-y-3 md:hidden">
+              {encontros.map((encontro) => (
+                <EncontroCard
+                  key={encontro.id}
+                  encontro={encontro}
+                  onStatusChange={(novoStatus) =>
+                    handleStatusChange(encontro.id, novoStatus)
+                  }
+                  isUpdatingStatus={isUpdatingStatus}
+                />
+              ))}
+            </div>
+
+            {/* Visualização desktop e tablet */}
+            <div className="hidden w-full overflow-x-auto rounded-xl border md:block">
+              <EncontroTable
+                encontros={encontros}
+                onStatusChange={handleStatusChange}
+                isUpdatingStatus={isUpdatingStatus} statusLabel={"agendado"} isTodas={false}              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
